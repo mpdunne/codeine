1f49c5ded1ec4f6c0acb04ccf9b8b5437dbfc347
Michael Dunne <6329303+mpdunne@users.noreply.github.com>
2026-06-05 15:34:42 +0100

    Add RNA option to CodonTable and make immutable

--- codeine/translation/tables.py
+++ codeine/translation/tables.py
@@ -1,36 +1,77 @@
-from typing import Dict, List
+from types import MappingProxyType
+from typing import Mapping
 
 from Bio.Data import CodonTable as TranslationTable
 
 
 class CodonTable:
     """
-    Basic CodonTable class, using the standard translation table with DNA and uniform probabilites.
+    Basic codon table using the standard translation table.
+
+    Uses DNA by default. If rna=True, codons are represented with U instead of T.
+    Codon probabilities are uniform within each amino acid.
     """
 
-    def __init__(self) -> None:
-        """
-        Constructor for the codon table class.
-        """
-        dna_to_aa = TranslationTable.unambiguous_dna_by_name["Standard"].forward_table
-        aa_to_dna = {}
+    def __init__(self, rna: bool = False) -> None:
+        self.rna = rna
+
+        dna_to_aa = dict(
+            TranslationTable.unambiguous_dna_by_name["Standard"].forward_table
+        )
+
+        aa_to_dna: dict[str, list[str]] = {}
         for codon, aa in dna_to_aa.items():
-            aa_to_dna[aa] = aa_to_dna.get(aa, []) + [codon]
+            aa_to_dna.setdefault(aa, []).append(codon)
 
-        codon_probabilities = {aa: {codon: 1 / len(codons) for codon in codons} for aa, codons in aa_to_dna.items()}
+        if rna:
+            codons_to_aa = {
+                self._normalise_codon(codon, rna=True): aa
+                for codon, aa in dna_to_aa.items()
+            }
+            aa_to_codons = {
+                aa: tuple(
+                    self._normalise_codon(codon, rna=True)
+                    for codon in codons
+                )
+                for aa, codons in aa_to_dna.items()
+            }
+        else:
+            codons_to_aa = {
+                self._normalise_codon(codon, rna=False): aa
+                for codon, aa in dna_to_aa.items()
+            }
+            aa_to_codons = {
+                aa: tuple(
+                    self._normalise_codon(codon, rna=False)
+                    for codon in codons
+                )
+                for aa, codons in aa_to_dna.items()
+            }
 
-        self._dna_to_aa = dna_to_aa
-        self._aa_to_dna = aa_to_dna
-        self._codon_probabilities = codon_probabilities
+        codon_probabilities = {
+            aa: {
+                codon: 1 / len(codons)
+                for codon in codons
+            }
+            for aa, codons in aa_to_codons.items()
+        }
 
-    @property
-    def codons_to_aa(self) -> Dict[str, str]:
-        return self._dna_to_aa
+        self.codons_to_aa = self._freeze_dict(codons_to_aa)
+        self.aa_to_codons = self._freeze_dict(aa_to_codons)
+        self.codon_probabilities = self._freeze_nested_dict(codon_probabilities)
 
-    @property
-    def aa_to_codons(self) -> Dict[str, List[str]]:
-        return self._aa_to_dna
+    @staticmethod
+    def _normalise_codon(codon: str, rna: bool = False) -> str:
+        codon = codon.upper()
+        return codon.replace("T", "U") if rna else codon.replace("U", "T")
 
-    @property
-    def codon_probabilities(self) -> Dict[str, Dict[str, float]]:
-        return self._codon_probabilities
+    @staticmethod
+    def _freeze_dict(d: Mapping):
+        return MappingProxyType(dict(d))
+
+    @staticmethod
+    def _freeze_nested_dict(d: Mapping):
+        return MappingProxyType({
+            key: MappingProxyType(dict(value))
+            for key, value in d.items()
+        })
