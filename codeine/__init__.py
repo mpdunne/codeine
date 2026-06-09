61136610bc067d42319d9e3c09a1fb834b78b469
Michael Dunne <6329303+mpdunne@users.noreply.github.com>
2026-06-09 15:31:35 +0200

    Imports in __init__.py

--- codeine/__init__.py
+++ codeine/__init__.py
@@ -1,5 +1,6 @@
 from codeine.sequence.space import CodingSpace
 from codeine.translation.tables import TranslationTable
+from codeine.translation.weights import CodonWeights
 
 
-__all__ = ['TranslationTable', 'CodingSpace']
+__all__ = ['CodingSpace', 'CodonWeights', 'TranslationTable']
