ef11ae6449d5e60e581b2f235c31938b5d838d7f
Michael Dunne <6329303+mpdunne@users.noreply.github.com>
2026-06-15 16:56:01 +0200

    Basic MutationSpace

---
+++ codeine/sequence/mutate.py
@@ -0,0 +1,149 @@
+from typing import Collection, Optional, Set
+
+from codeine.sequence.space import CodingSpace
+
+
+class MutationSpace:
+    """
+    Represents the set of valid coding sequences that can be reached by
+    mutating a reference CDS in a given CodingSpace, subject to constraints.
+
+    A MutationSpace is defined by:
+
+    - A CodingSpace containing the global sequence constraints.
+    - A reference CDS.
+    - A set of codon positions that are free to mutate.
+
+    Positions that are not free are temporarily considered frozen and
+    will remain identical to the reference CDS.
+    """
+
+    def __init__(self,
+                 space: CodingSpace,
+                 cds: str,
+                 free_positions: Optional[Collection[int]] = None,
+                 ):
+        """
+        Constructor for the MutationSpace class.
+
+        Parameters
+        ----------
+        space
+            The CodingSpace object to which this given sequence should belong.
+        cds
+            The parent/reference CDS.
+        free_positions
+            Which positions are allowed to change?
+        """
+        self.space = space
+        self.cds = cds
+
+        if free_positions is None:
+            free_positions = range(1, len(space.view.aa_seq) + 1)
+
+        self._free_positions: Set[int] = set()
+        self.set_free_positions(free_positions)
+
+    @property
+    def free_positions(self):
+        """
+        Codon positions that are currently free to mutate.
+        """
+        return frozenset(self._free_positions)
+
+    @property
+    def frozen_positions(self):
+        """
+        Codon positions that are currently fixed to the reference CDS.
+        """
+        all_positions = set(range(1, len(self.space.view.aa_seq) + 1))
+        return frozenset(all_positions - self._free_positions)
+
+    def _validate_positions(self, positions: Collection[int]) -> Set[int]:
+        """
+        Validate a collection of codon positions.
+        """
+        positions = set(positions)
+        invalid = [pos for pos in positions if pos < 1 or pos > len(self.space.view.aa_seq)]
+        if invalid:
+            raise ValueError(f'Invalid codon positions: {sorted(invalid)}')
+
+        return positions
+
+    def _codon_at_position(self, pos: int) -> str:
+        """
+        Get the codon of the reference CDS at the specified position.
+
+        Parameters
+        ----------
+        pos
+            The position in the AA sequence.
+
+        Returns
+        -------
+        A codon.
+        """
+        return self.cds[3 * (pos - 1): 3 * pos]
+
+    def _update_pins(self) -> None:
+        """
+        Update the pins on the underlying space.
+        """
+        pins = {pos: self._codon_at_position(pos) for pos in self.frozen_positions}
+        self.space.set_pinned_codons(pins)
+
+    @property
+    def n_valid_variants(self) -> int:
+        """
+        Number of valid variants under the current mutation constraints.
+        """
+        return self.space.n_valid_sequences
+
+    def set_free_positions(self, positions: Collection[int]) -> None:
+        """
+        Replace the current set of free positions.
+        """
+        self._free_positions = self._validate_positions(positions)
+        self._update_pins()
+
+    def freeze_positions(self, positions: Collection[int]) -> None:
+        """
+        Freeze the given codon positions.
+        """
+        positions = self._validate_positions(positions)
+
+        self._free_positions -= positions
+        self._update_pins()
+
+    def unfreeze_positions(self, positions: Collection[int]) -> None:
+        """
+        Unfreeze the given codon positions.
+        """
+        positions = self._validate_positions(positions)
+
+        self._free_positions |= positions
+        self._update_pins()
+
+    def freeze_all(self) -> None:
+        """
+        Freeze all codon positions.
+        """
+        self._free_positions.clear()
+        self._update_pins()
+
+    def unfreeze_all(self) -> None:
+        """
+        Unfreeze all codon positions.
+        """
+        self._free_positions = set(range(1, len(self.space.view.aa_seq) + 1))
+        self._update_pins()
+
+    def sample(self) -> str:
+        """
+        Sample a variant from this mutation space.
+
+        Returns
+        -------
+        A sampled string sequence from this mutation space.
+        """
+        return self.space.sample()
---
+++ test/sequence/test_mutate.py
@@ -0,0 +1,146 @@
+import pytest
+from unittest.mock import MagicMock
+
+from codeine.sequence.space import CodingSpace
+from codeine.sequence.mutate import MutationSpace
+
+
+def test_mutation_space_defaults_to_all_positions_free():
+    space = CodingSpace('MIKEY')
+    cds = space[0]
+    muts = MutationSpace(space, cds)
+
+    assert muts.free_positions == frozenset(range(1, len(space.view.aa_seq) + 1))
+    assert muts.frozen_positions == frozenset()
+
+
+def test_mutation_space_can_set_free_positions():
+    space = CodingSpace('MIKEY')
+    cds = space[0]
+    muts = MutationSpace(space, cds)
+
+    muts.set_free_positions([1, 3])
+
+    assert muts.free_positions == frozenset({1, 3})
+    assert muts.frozen_positions == frozenset({2, 4, 5})
+
+
+def test_mutation_space_can_freeze_positions():
+    space = CodingSpace('MIKEY')
+    cds = space[0]
+    muts = MutationSpace(space, cds)
+
+    muts.freeze_positions([1])
+
+    assert 1 not in muts.free_positions
+    assert 1 in muts.frozen_positions
+
+
+def test_mutation_space_can_unfreeze_positions():
+    space = CodingSpace('MIKEY')
+    cds = space[0]
+    muts = MutationSpace(space, cds, free_positions=[3])
+
+    muts.unfreeze_positions([1])
+
+    assert muts.free_positions == frozenset({1, 3})
+
+
+def test_mutation_space_can_freeze_all():
+    space = CodingSpace('MIKEY')
+    cds = space[0]
+    muts = MutationSpace(space, cds)
+
+    muts.freeze_all()
+
+    assert muts.free_positions == frozenset()
+    assert muts.frozen_positions == frozenset(range(1, len(space.view.aa_seq) + 1))
+
+
+def test_mutation_space_can_unfreeze_all():
+    space = CodingSpace('MIKEY')
+    cds = space[0]
+    muts = MutationSpace(space, cds, free_positions=[])
+
+    muts.unfreeze_all()
+
+    assert muts.free_positions == frozenset(range(1, len(space.view.aa_seq) + 1))
+    assert muts.frozen_positions == frozenset()
+
+
+def test_mutation_space_rejects_invalid_positions():
+    space = CodingSpace('MIKEY')
+    cds = space[0]
+    muts = MutationSpace(space, cds)
+
+    with pytest.raises(ValueError):
+        muts.set_free_positions([-1])
+
+    with pytest.raises(ValueError):
+        muts.set_free_positions([len(space.view.aa_seq) + 1])
+
+
+def test_mutation_space_updates_pins_when_free_positions_change():
+    space = CodingSpace('MIKEY')
+    cds = space[0]
+
+    muts = MutationSpace(space, cds)
+    muts.set_free_positions([2])
+
+    expected_pins = {1: [cds[0:3]], 3: [cds[6:9]], 4: [cds[9:12]], 5: [cds[12:15]]}
+    assert space.view.pinned_codons == expected_pins
+
+
+def test_mutation_space_freeze_all_pins_every_codon():
+    space = CodingSpace('MIKEY')
+    cds = space.sample()
+    muts = MutationSpace(space, cds)
+
+    muts.freeze_all()
+
+    expected_pins = {
+        pos: [cds[3 * (pos - 1): 3 * pos]]
+        for pos in range(1, len(space.view.aa_seq) + 1)
+    }
+
+    assert space.view.pinned_codons == expected_pins
+
+
+def test_mutation_space_unfreeze_all_clears_pins():
+    space = CodingSpace('MIKEY')
+    cds = space.sample()
+    muts = MutationSpace(space, cds, free_positions=[])
+
+    muts.unfreeze_all()
+    assert space.view.pinned_codons == {}
+
+
+def test_mutation_space_sample_uses_current_pins():
+    space = CodingSpace('MIKEY')
+    cds = space.sample()
+
+    muts = MutationSpace(space, cds, free_positions=[2])
+    variants = [muts.sample() for _ in range(1000)]
+    assert len(set(variants)) == muts.n_valid_variants == 3
+    for variant in variants:
+        assert variant[0:3] == cds[0:3]
+        assert variant[6:9] == cds[6:9]
+        assert variant[9:12] == cds[9:12]
+        assert variant[12:15] == cds[12:15]
+
+    muts = MutationSpace(space, cds, free_positions=[2, 3])
+    variants = [muts.sample() for _ in range(1000)]
+    assert len(set(variants)) == muts.n_valid_variants == 6
+    for variant in variants:
+        assert variant[0:3] == cds[0:3]
+        assert variant[9:12] == cds[9:12]
+        assert variant[12:15] == cds[12:15]
+
+    muts = MutationSpace(space, cds, free_positions=[5])
+    variants = [muts.sample() for _ in range(1000)]
+    assert len(set(variants)) == muts.n_valid_variants == 2
+    for variant in variants:
+        assert variant[0:3] == cds[0:3]
+        assert variant[3:6] == cds[3:6]
+        assert variant[6:9] == cds[6:9]
+        assert variant[9:12] == cds[9:12]
