8c35fea38b50d651effd93a311245c5a4efc106d
Michael Dunne <6329303+mpdunne@users.noreply.github.com>
2026-06-17 14:13:14 +0200

    First version of banned sequences

--- test/sequence/test_space.py
+++ test/sequence/test_space.py
@@ -4,7 +4,6 @@ import random
 
 from Bio.Seq import Seq
 
-from codeine.sequence.view import CodonGraphView
 from codeine.sequence.space import CodingSpace
 from codeine.motifs.restriction import RestrictionSite
 
@@ -238,11 +237,6 @@ def test_space_contains():
         assert seq + 'ATG' not in space
 
 
-@pytest.fixture
-def disable_banned_sequence_filtering(monkeypatch):
-    monkeypatch.setattr(CodonGraphView, '_check_banned_sequence_support', lambda self: None)
-
-
 def test_expand_forbidden_motifs_none():
     assert CodingSpace._expand_and_validate_forbidden_motifs(None, rna=False) == []
 
@@ -310,17 +304,17 @@ def test_validate_max_homopolymer_rejects_less_than_one():
         CodingSpace._expand_and_validate_max_homopolymer(0)
 
 
-def test_forbidden_motifs_are_stored(disable_banned_sequence_filtering):
+def test_forbidden_motifs_are_stored():
     space = CodingSpace('MIKEY', forbidden_motifs=[RestrictionSite.EcoRI, 'AAAA'])
     assert space.forbidden_sequences == ['AAAA', 'GAATTC']
 
 
-def test_max_homopolymer_is_stored(disable_banned_sequence_filtering):
+def test_max_homopolymer_is_stored():
     space = CodingSpace('MIKEY', max_homopolymer=4)
     assert space.max_homopolymer == 4
 
 
-def test_max_homopolymer_is_expanded_correctly(disable_banned_sequence_filtering):
+def test_max_homopolymer_is_expanded_correctly():
     space = CodingSpace('MIKEY', max_homopolymer=None)
     assert not space.forbidden_sequences
 
@@ -328,12 +322,12 @@ def test_max_homopolymer_is_expanded_correctly(disable_banned_sequence_filtering
     assert all(nt * 5 in space.forbidden_sequences for nt in 'ACGT')
 
 
-def test_mixed_restrictions(disable_banned_sequence_filtering):
+def test_mixed_restrictions():
     space = CodingSpace('MIKEY', max_homopolymer=4, forbidden_motifs = [RestrictionSite.BsaI, 'GGTTCC'])
     assert set(space.forbidden_sequences) == {'GAGACC', 'GGTCTC', 'GGTTCC', 'AAAAA', 'CCCCC', 'GGGGG', 'TTTTT'}
 
 
-def test_forbidden_motifs_repr(disable_banned_sequence_filtering):
+def test_forbidden_motifs_repr():
     space = CodingSpace('MIKEY', forbidden_motifs=[RestrictionSite.EcoRI, 'AAAA'],)
 
     text = repr(space)
@@ -343,7 +337,7 @@ def test_forbidden_motifs_repr(disable_banned_sequence_filtering):
     assert 'AAAA' in text
 
 
-def test_max_homopolymer_repr(disable_banned_sequence_filtering):
+def test_max_homopolymer_repr():
     space = CodingSpace('MIKEY', max_homopolymer=4)
 
     text = repr(space)
@@ -410,7 +404,7 @@ def test_coding_space_pickle_preserves_pins():
     assert [*loaded.enumerate()] == [*space.enumerate()]
 
 
-def test_coding_space_pickle_preserves_constraints(disable_banned_sequence_filtering):
+def test_coding_space_pickle_preserves_constraints():
     space = CodingSpace(
         'MIKEY',
         codon_restrictions={2: 'ATC'},
