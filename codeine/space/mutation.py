182474c705f90f5a5054a7be5bcec960b86545a3
Michael Dunne <6329303+mpdunne@users.noreply.github.com>
2026-07-23 12:10:04 +0200

    Error message when calling len

--- codeine/space/mutation.py
+++ codeine/space/mutation.py
@@ -118,6 +118,16 @@ class MutationSpace:
         """
         return seq in self.view
 
+    def __len__(self) -> int:
+        """
+        This method exists only to provide a helpful error message. Being able to call
+        len(space) is a totally reasonable thing to expect, but python's ``len`` hits a limit
+        for very large spaces.
+
+        Use ``count()`` or ``n_valid_sequences`` instead.
+        """
+        raise TypeError('len() is not supported for MutationSpace; use .count() or .n_valid_variants instead.')
+
     def __repr__(self) -> str:
         molecule = 'RNA' if self.translation_table.rna else 'DNA'
 
