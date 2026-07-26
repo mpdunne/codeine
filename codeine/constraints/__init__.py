452b3398a74dbd3b8b9e41dfd85a2164b10bf99a
Michael Dunne <6329303+mpdunne@users.noreply.github.com>
2026-07-26 19:09:47 +0200

    Add HomopolymerConstraint

---
+++ codeine/constraints/__init__.py
@@ -0,0 +1,7 @@
+from codeine.constraints.banned import BannedSequenceConstraint
+from codeine.constraints.homopolymer import HomopolymerConstraint
+
+__all__ = [
+    'BannedSequenceConstraint',
+    'HomopolymerConstraint',
+]
---
+++ codeine/constraints/homopolymer.py
@@ -0,0 +1,25 @@
+from codeine.constraints.banned import BannedSequenceConstraint
+
+
+class HomopolymerConstraint(BannedSequenceConstraint):
+    """
+    Exclude homopolymers longer than a specified maximum length.
+    """
+
+    def __init__(self, max_length: int) -> None:
+        """
+        Parameters
+        ----------
+        max_length
+            Maximum permitted number of consecutive identical nucleotides.
+        """
+        if not isinstance(max_length, int):
+            raise TypeError('max_length must be an integer.')
+
+        if max_length < 1:
+            raise ValueError('max_length must be at least 1.')
+
+        self.max_length = max_length
+
+        banned_homopolymers = [nt * (max_length + 1) for nt in 'ACGT']
+        super().__init__(banned_homopolymers)
