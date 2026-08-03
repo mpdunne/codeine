93e9435aaa3825ea32c5eb7634b250789e571785
Michael Dunne <6329303+mpdunne@users.noreply.github.com>
2026-08-03 10:01:49 +0200

    Import new constraints into codeine.constraints

--- codeine/constraints/__init__.py
+++ codeine/constraints/__init__.py
@@ -1,9 +1,16 @@
 from codeine.constraints.motifs import ForbiddenMotifConstraint
 from codeine.constraints.homopolymers import HomopolymerConstraint
 from codeine.constraints.tandem import TandemRepeatConstraint
+from codeine.constraints.hairpins import HairpinConstraint
+from codeine.constraints.repeats import DirectRepeatConstraint
+from codeine.constraints.repeats import InvertedRepeatConstraint
+
 
 __all__ = [
     'ForbiddenMotifConstraint',
     'HomopolymerConstraint',
     'TandemRepeatConstraint',
+    'InvertedRepeatConstraint',
+    'DirectRepeatConstraint',
+    'HairpinConstraint',
 ]
